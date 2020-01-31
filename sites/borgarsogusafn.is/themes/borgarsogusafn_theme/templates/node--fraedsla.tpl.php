381741d44c9ef70b0647d6326fa7aeb36553b362
Hilmar Kári Hallbjornsson <drupalviking@gmail.com>
2020-01-31 13:26:21 +0000

    Removed date display from template

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--fraedsla.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--fraedsla.tpl.php
@@ -10,7 +10,7 @@
       ?>
    </div>
 
-<?php 
+<?php
    hide($content['field_aldur']);
    hide($content['field_summary']);
 ?>
@@ -19,10 +19,10 @@
       <?php if (block_get_blocks_by_region('custom_breadcrumbs')): ?>
          <?php print render(block_get_blocks_by_region('custom_breadcrumbs')); ?>
       <?php endif; ?>
-      <div class="date-wrapper">
-         <span class="where"><?php print render($content['field_aldur']); ?></span>
-         <?php print $formatted_date; ?>
-      </div>
+      <!--div class="date-wrapper">
+         <span class="where"><?php //print render($content['field_aldur']); ?></span>
+         <?php //print $formatted_date; ?>
+      </div-->
       <h1 class="title f-XXL f-2 text-dark-grey"><?php print $node->title; ?></h1>
 	  <h2 class="f-M f-2-2 subtitle"><?php print render($content['field_summary']); ?></h2>
 	<?php print render($content); ?>
