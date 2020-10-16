b3364c973fb3e41ffe223423c547e7ea565e11a7
Hilmar The Drupalviking Hallbjörnsson <drupalviking@gmail.com>
2020-10-16 16:04:35 +0000

    Added tagging and other styles for reports

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
@@ -1,6 +1,15 @@
 <div class="main reports">
-  <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
+  <div class="report-category"><span><?=render($content['field_tegund_skyrslu']);?></span></div>
   <h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2>
+  <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
+  <div class="tags tagwords">
+    <p><?=render($content['field_efnisord']); ?></p>
+  </div>
+  <div class="tags authors">
+
+    <p><?=count($content['field_skyrsluhofundur']['#items']) > 1 ? 'Höfundar: ' : 'Höfundur: '; ?><?=render($content['field_skyrsluhofundur']); ?></p>
+  </div>
+
   <div class="report">
     <?php print render($content['field_efni']); ?>
   </div>
