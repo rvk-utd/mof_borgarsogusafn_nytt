eff26d5d66c7b8ea2b933719fcf37f5a6694b735
Hilmar The Drupalviking Hallbjörnsson <drupalviking@gmail.com>
2020-10-16 16:37:10 +0000

    Added a wrapper for all data, to exclude if no data

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
@@ -1,6 +1,7 @@
 <div class="main reports">
   <div class="report-category"><span><?=render($content['field_tegund_skyrslu']);?></span></div>
-  <h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2>
+
+  <?php if($content['field_skyrsla_nr']): ?><h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2><?php endif; ?>
   <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
   <div class="tags tagwords">
     <p><?=render($content['field_efnisord']); ?></p>
