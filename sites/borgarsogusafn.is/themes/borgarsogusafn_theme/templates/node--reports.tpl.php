7ed3da379a487cb14ce07328a3c125183b034ed0
Hilmar The Drupalviking Hallbjörnsson <drupalviking@gmail.com>
2020-10-16 10:09:40 +0000

    Added new template for reports display

---
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
@@ -0,0 +1,8 @@
+<div class="main reports">
+  <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
+  <h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2>
+  <div class="report">
+    <?php print render($content['field_efni']); ?>
+  </div>
+
+</div>
