af1da32d322b15d35a3b27063752b4275608f900
Hilmar The Drupalviking Hallbjörnsson <drupalviking@gmail.com>
2020-10-16 16:37:17 +0000

    Added a wrapper for all data, to exclude if no data

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
@@ -3,16 +3,17 @@
 
   <?php if($content['field_skyrsla_nr']): ?><h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2><?php endif; ?>
   <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
+  <?php if($content['field_efnisord']): ?>
   <div class="tags tagwords">
     <p><?=render($content['field_efnisord']); ?></p>
   </div>
+  <?php endif; ?>
+  <?php if($content['field_skyrsluhofundur']): ?>
   <div class="tags authors">
-
     <p><?=count($content['field_skyrsluhofundur']['#items']) > 1 ? 'Höfundar: ' : 'Höfundur: '; ?><?=render($content['field_skyrsluhofundur']); ?></p>
   </div>
-
+  <?php endif; ?>
   <div class="report">
     <?php print render($content['field_efni']); ?>
   </div>
-
 </div>
