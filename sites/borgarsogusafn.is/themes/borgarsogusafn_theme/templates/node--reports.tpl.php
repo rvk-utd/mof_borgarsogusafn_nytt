77cb4c6d635536fb45d329054b1fe9eb4ce9eb4f
Hilmar Kári Hallbjörnsson <drupalviking@gmail.com>
2021-07-01 10:41:19 +0000

    Report layout changes (#15)

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/node--reports.tpl.php
@@ -1,6 +1,7 @@
 <div class="main reports">
-  <div class="report-category"><span><?=render($content['field_tegund_skyrslu']);?></span></div>
-
+  <?php if($content['field_tegund_skyrslu']):?>
+  <div class="report-category"><?=render($content['field_tegund_skyrslu']);?></div>
+  <?php endif; ?>
   <?php if($content['field_skyrsla_nr']): ?><h2 class="f-M f-2-2 subtitle">Skýrsla: <?=render($content['field_skyrsla_nr']);?></h2><?php endif; ?>
   <h1 class="title f-XXL f-2 text-dark-grey"><?php print $title; ?></h1>
   <?php if($content['field_efnisord']): ?>
