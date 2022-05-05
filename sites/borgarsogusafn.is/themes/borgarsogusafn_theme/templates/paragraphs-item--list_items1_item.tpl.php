14071756113b57c9f421bd1ab341c7dac2bdaf81
Rafal Miszkiniuk <rmiszkiniuk@sii.pl>
2022-05-05 15:14:13 +0200

    fix grid styles

--- sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/paragraphs-item--list_items1_item.tpl.php
+++ sites/borgarsogusafn.is/themes/borgarsogusafn_theme/templates/paragraphs-item--list_items1_item.tpl.php
@@ -1,11 +1,11 @@
 <div class="item">
 	<a href="<?php if ($content['field_link']): ?><?php print render($content['field_link']); ?><?php endif; ?>">
-		<div class="image-wrapper match-height">
+		<div class="image-wrapper">
 			<?php if ($content['field_mynd']): ?>
 				<?php print render($content['field_mynd']); ?>
 			<?php endif; ?>
 		</div>
-		<div class="text-wrapper match-height">
+		<div class="text-wrapper">
 			<?php if ($content['field_title']): ?>
 				<h2 class="f-M f-2 text-white"><?php print render($content['field_title']); ?></h2>
 			<?php endif; ?>
